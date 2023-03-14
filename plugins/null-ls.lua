a58d24815f7f4e9c83a2928d9518e12ce92b5387
Kai Forsthövel <kai.forsthoevel@invision.de>
2023-03-14 15:12:20 +0100

    Configure null-ls plugin

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -2,15 +2,28 @@ return {
   "jose-elias-alvarez/null-ls.nvim",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
 
     -- Check supported formatters and linters
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
     config.sources = {
-      -- Set a formatter
-      -- null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.formatting.prettier.with {
+        filetypes = { "html", "json", "yaml", "markdown", "ruby" },
+      },
+      null_ls.builtins.formatting.black,
+      null_ls.builtins.formatting.isort,
+      null_ls.builtins.formatting.clang_format,
+      null_ls.builtins.formatting.rustfmt,
+      null_ls.builtins.formatting.shfmt.with {
+        args = { "-i", "2" },
+      },
+      null_ls.builtins.diagnostics.luacheck,
+      null_ls.builtins.diagnostics.flake8,
+      null_ls.builtins.diagnostics.vale.with {
+        extra_args = { "--config=/Users/kai/.config/vale/.vale.ini" },
+      },
     }
     return config -- return final config table
   end,
