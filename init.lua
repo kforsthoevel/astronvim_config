caa46748a68f892d612ac55f80bdf2a36fe4ee54
Kai Forsthövel <kai.forsthoevel@invision.de>
2023-03-14 15:15:57 +0100

    Set filetype for terraform and Jenkinsfile

--- init.lua
+++ init.lua
@@ -69,16 +69,16 @@ return {
   -- anything that doesn't fit in the normal config locations above can go here
   polish = function()
     -- Set up custom filetypes
-    -- vim.filetype.add {
-    --   extension = {
-    --     foo = "fooscript",
-    --   },
-    --   filename = {
-    --     ["Foofile"] = "fooscript",
-    --   },
-    --   pattern = {
-    --     ["~/%.config/foo/.*"] = "fooscript",
-    --   },
-    -- }
+    vim.filetype.add {
+      extension = {
+        tf = "hcl",
+      },
+      filename = {
+        ["Jenkinsfile"] = "groovy",
+      },
+      -- pattern = {
+      --   ["~/%.config/foo/.*"] = "fooscript",
+      -- },
+    }
   end,
 }
