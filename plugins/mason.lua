cd0098f0caab9f82181bb289888c20011d71e66b
Kai Forsthövel <kai.forsthoevel@invision.de>
2023-03-14 15:13:13 +0100

    Configure mason plugin

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,7 +5,15 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      -- ensure_installed = { "lua_ls" },
+      ensure_installed = {
+        "bashls",
+        "lua_ls",
+        "pyright",
+        "solargraph",
+        "terraformls",
+        "tflint",
+        "tsserver",
+      },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
