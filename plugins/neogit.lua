a3a14c2a09df4775504669ff6865f5bc05c505ed
Kai Forsthövel <kai.forsthoevel@invision.de>
2023-03-23 08:50:44 +0100

    Only load neogit when needed and add keymap

--- plugins/neogit.lua
+++ plugins/neogit.lua
@@ -1,18 +1,19 @@
 return {
   "TimUntersberger/neogit",
-  {
-    "TimUntersberger/neogit",
-    config = function()
-      require("neogit").setup {
-        integrations = {
-          diffview = true,
-        },
-      }
-    end,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "sindrets/diffview.nvim",
-    },
-    lazy = false,
+  config = function()
+    require("neogit").setup {
+      disable_commit_confirmation = true,
+      integrations = {
+        diffview = true,
+      },
+    }
+  end,
+  keys = {
+    { "<leader>gn", "<cmd>Neogit<cr>", desc = "Neogit" },
   },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+  },
+  cmd = "Neogit",
 }
