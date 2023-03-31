08cc5eba0c39134ee85f7d1265658c1eda1eac75
Kai Forsthövel <kai.forsthoevel@invision.de>
2023-03-31 10:42:32 +0200

    Set filetype handling for ansible and NeogitCommitMessage

--- init.lua
+++ init.lua
@@ -74,12 +74,30 @@ return {
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
   polish = function()
+    local function yaml_ft(path, bufnr)
+      -- get content of buffer as string
+      local content = vim.filetype.getlines(bufnr)
+      if type(content) == "table" then content = table.concat(content, "\n") end
+
+      -- check if file is in roles, tasks, or handlers folder
+      local path_regex = vim.regex "(tasks\\|roles\\|handlers)/"
+      if path_regex and path_regex:match_str(path) then return "yaml.ansible" end
+      -- check for known ansible playbook text and if found, return yaml.ansible
+      local regex = vim.regex "hosts:\\|tasks:"
+      if regex and regex:match_str(content) then return "yaml.ansible" end
+
+      -- return yaml if nothing else
+      return "yaml"
+    end
+
     -- Set up custom filetypes
     vim.filetype.add {
       extension = {
         -- tf = "terraform",
         -- tfvars = "terraform",
         tfstate = "json",
+        yml = yaml_ft,
+        yaml = yaml_ft,
       },
       filename = {
         ["Jenkinsfile"] = "groovy",
@@ -90,5 +108,6 @@ return {
     }
     vim.cmd [[autocmd Filetype gitcommit setlocal formatprg=par\ -qw72 spell spelllang=en_us]]
     vim.cmd [[autocmd Filetype markdown setlocal formatprg=par\ -qw72 spell spelllang=en_us]]
+    vim.cmd [[autocmd Filetype NeogitCommitMessage setlocal formatprg=par\ -qw72 spell spelllang=en_us]]
   end,
 }
