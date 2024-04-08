6bde314668d828a20b8d9bc3f30107e9db30bef7
Kai Forsthövel <kai.forsthoevel@invision.de>
2024-04-08 15:19:45 +0200

    Migrate from null-ls to none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- Customize None-ls sources
 
@@ -7,15 +7,23 @@ return {
   "nvimtools/none-ls.nvim",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
 
     -- Check supported formatters and linters
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
     config.sources = {
-      -- Set a formatter
-      -- null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.formatting.prettier.with {
+        filetypes = { "html", "json", "yaml", "markdown", "ruby" },
+      },
+      null_ls.builtins.formatting.shfmt.with {
+        args = { "-i", "2" },
+      },
+      -- null_ls.builtins.diagnostics.luacheck,
+      null_ls.builtins.diagnostics.vale.with {
+        extra_args = { "--config=/Users/kai/.config/vale/.vale.ini" },
+      },
     }
     return config -- return final config table
   end,
