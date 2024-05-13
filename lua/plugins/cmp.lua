51bd7139ec01658356052a2bc55ca38945d65da2
Kai Forsthövel <kai.forsthoevel@invision.de>
2024-05-13 09:20:02 +0200

    Make cmp work with cmp-git

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,46 +1,46 @@
--- local cmp = require "cmp"
-
-return {
+return { -- override nvim-cmp plugin
   "hrsh7th/nvim-cmp",
-  opts = {
-    -- performance = {
-    --   debounce = 300,
-    --   throttle = 120,
-    --   fetching_timeout = 100
-    -- },
-    -- experimental = {
-    --   ghost_text = true,
-    -- },
-    sources = {
+  dependencies = {
+    "petertriho/cmp-git",
+  },
+
+  -- override the options table that is used in the `require("cmp").setup()` call
+  opts = function(_, opts)
+    -- opts parameter is the default options table
+    -- the function is lazy loaded so cmp is able to be required
+    local cmp = require "cmp"
+    -- modify the sources part of the options table
+    opts.sources = cmp.config.sources {
       { name = "nvim_lsp", priority = 1000 },
-      { name = "luasnip", priority = 700 },
-      { name = "path", priority = 650 },
+      { name = "luasnip", priority = 750 },
+      { name = "path", priority = 600 },
+      {
+        name = "git",
+        priority = 850,
+        option = {
+          filetypes = { "gitcommit", "neogitcommitmessage", "NeogitCommitMessage" },
+        },
+      },
       {
         name = "buffer",
-        priority = 400,
+        priority = 500,
         option = {
           get_bufnrs = function() return vim.api.nvim_list_bufs() end,
         },
       },
-    },
-  },
-  -- cmp.setup.cmdline({ "/", "?" }, {
-  --   mapping = cmp.mapping.preset.cmdline(),
-  --   sources = {
-  --     { name = "buffer" },
-  --   },
-  -- }),
-  -- cmp.setup.cmdline(":", {
-  --   mapping = cmp.mapping.preset.cmdline(),
-  --   sources = cmp.config.sources({
-  --     { name = "path" },
-  --   }, {
-  --     {
-  --       name = "cmdline",
-  --       option = {
-  --         ignore_cmds = { "Man", "!" },
-  --       },
-  --     },
-  --   }),
-  -- }),
+    }
+    cmp.setup.filetype({ "gitcommit" }, {
+      sources = cmp.config.sources {
+        {
+          name = "git",
+        },
+      },
+    })
+    require("cmp_git").setup {
+      filetypes = { "gitcommit", "NeogitCommitMessage" },
+      github = {
+        hosts = { "github.com" },
+      },
+    }
+  end,
 }
