8f718da5e3d26c752456011e925363eb1faac7cd
Kai Forsthövel <kai.forsthoevel@invision.de>
2024-08-29 17:39:10 +0200

    Improve yaml file handling

--- lua/polish.lua
+++ lua/polish.lua
@@ -4,21 +4,21 @@
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
-local function yaml_ft(path, bufnr)
-  -- get content of buffer as string
-  local content = vim.filetype.getlines(bufnr)
-  if type(content) == "table" then content = table.concat(content, "\n") end
-
-  -- check if file is in roles, tasks, or handlers folder
-  local path_regex = vim.regex "(tasks\\|roles\\|handlers)/"
-  if path_regex and path_regex:match_str(path) then return "yaml.ansible" end
-  -- check for known ansible playbook text and if found, return yaml.ansible
-  local regex = vim.regex "hosts:\\|tasks:"
-  if regex and regex:match_str(content) then return "yaml.ansible" end
-
-  -- return yaml if nothing else
-  return "yaml"
-end
+-- local function yaml_ft(path, bufnr)
+--   -- get content of buffer as string
+--   local content = vim.filetype.getlines(bufnr)
+--   if type(content) == "table" then content = table.concat(content, "\n") end
+--
+--   -- check if file is in roles, tasks, or handlers folder
+--   local path_regex = vim.regex "(tasks\\|roles\\|handlers)/"
+--   if path_regex and path_regex:match_str(path) then return "yaml.ansible" end
+--   -- check for known ansible playbook text and if found, return yaml.ansible
+--   local regex = vim.regex "hosts:\\|tasks:"
+--   if regex and regex:match_str(content) then return "yaml.ansible" end
+--
+--   -- return yaml if nothing else
+--   return "yaml"
+-- end
 
 -- Set up custom filetypes
 vim.filetype.add {
@@ -26,11 +26,18 @@ vim.filetype.add {
     -- tf = "terraform",
     -- tfvars = "terraform",
     tfstate = "json",
-    yml = yaml_ft,
-    yaml = yaml_ft,
+    -- yml = yaml_ft,
+    -- yaml = yaml_ft,
+    yml = "yaml",
+    yaml = "yaml",
+    hujson = "json",
   },
   filename = {
     ["Jenkinsfile"] = "groovy",
+  },
+  pattern = {
+    [".*/tasks/.*.yml"] = "yaml.ansible",
+    [".*/tasks/.*.yaml"] = "yaml.ansible",
   },
   -- extension = {
   --   foo = "fooscript",
